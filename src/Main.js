c864b535c908271e6fcca2ab4a9a8ea5c4e4e2a0
Merijn <prim@pr1mula.nl>
2023-01-09 15:14:47 +0100

    new version of react router

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {
   Route,
+  Routes,
   NavLink,
   HashRouter
 } from "react-router-dom";
 import Home from "./Home";
-import toDoDisplay from "./components/toDoDisplay.js";
+import ToDoDisplay from "./components/toDoDisplay.js";
 import Contact from "./Contact.js";
 import FeedDisplay from "./components/FeedDisplay.js";
 
@@ -19,16 +20,17 @@ class Main extends Component {
           <h1>Simple SPA</h1>
           <ul className="header">
             <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/toDoDisplay">To Do</NavLink></li>
+            <li><NavLink to="/tododisplay">To Do</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
-            <li><NavLink to="/FeedDisplay">Reddit Feed</NavLink></li>
+            <li><NavLink to="/feeddisplay">Reddit Feed</NavLink></li>
           </ul>
           <div className="content">
-          <Route exact path="/" component={Home}/>
-          <Route path="/toDoDisplay" component={toDoDisplay}/>
-          <Route path="/contact" component={Contact}/>
-          <Route path="/FeedDisplay" component={FeedDisplay}/>
-             
+          <Routes> 
+          <Route exact path="/" element={<Home />} />
+          <Route path="/ToDoDisplay" element={<ToDoDisplay />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/FeedDisplay" element={<FeedDisplay />}/>
+          </Routes>
           </div>
         </div>
         </HashRouter>
